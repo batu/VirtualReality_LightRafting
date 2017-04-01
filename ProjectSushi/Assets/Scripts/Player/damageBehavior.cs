85d4041f4a39576a5ae017a3f0f4700bccb5c4f0
batu <ba921@nyu.edu>
2017-04-01 12:52:13 +0400

    initial proc gen, initial damage system, color changing

---
+++ ProjectSushi/Assets/Scripts/Player/damageBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damageBehavior : MonoBehaviour {
+
+
+    public int maximumHealth = 3;
+    int currentHealth;
+
+    public GameObject model;
+    Material modelMaterial;
+
+    Color[] colors = { Color.green, Color.yellow, Color.red };
+
+    void OnCollisionEnter(Collision col) {
+
+        if (col.collider.tag == "Obstacle" ) {
+            currentHealth -= 1;
+            col.collider.tag = "Collided_Obstacle";
+        }
+        if(!(currentHealth < 1)) modelMaterial.color = colors[3 - currentHealth];
+
+        if(currentHealth == 0) {
+            Destroy(model);
+        }
+    }
+	// Use this for initialization
+	void Start () {
+        currentHealth = maximumHealth;
+        modelMaterial = model.GetComponent<Renderer>().material;
+        modelMaterial.color = colors[3 - maximumHealth];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ProjectSushi/Assets/Scripts/ProceduralGeneration/PrototypeProcGen.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrototypeProcGen : MonoBehaviour {
+
+    public float spawnTreshold;
+
+    GameObject obstacleHolder;
+    public GameObject obstacle;
+    public float initialSpawnDistance = 100f;
+
+    public float spawnLeftRightRange = 10f;
+    public float spawnMaxStep = 5f;
+    public float spawnMinStep = 1f;
+
+	// Use this for initialization
+	void Start () {
+        obstacleHolder = GameObject.FindGameObjectWithTag("Obstacle_Holder");
+        spawnInitialObstacles();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+
+    void spawnInitialObstacles() {
+        for (float spawn_z = 0; spawn_z < initialSpawnDistance;) {
+            GameObject toInstantiate = obstacle;
+            toInstantiate.transform.localScale = new Vector3(Random.Range(1,5), Random.Range(1, 5), Random.Range(1, 5));
+
+            spawn_z += Random.Range(spawnMinStep, spawnMaxStep);
+
+            Vector3 instantiatePosition = new Vector3(  Random.Range( -spawnLeftRightRange, spawnLeftRightRange), 
+                                                        transform.position.y, 
+                                                        transform.position.z + spawn_z);
+
+            Instantiate(toInstantiate, instantiatePosition, Random.rotationUniform);
+        }
+    }
+}
