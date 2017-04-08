d1d2db4d014d23e511f002da6ddab83a4f06da72
batu <ba921@nyu.edu>
2017-04-08 20:47:08 +0400

    added paddling code

--- ProjectSushi/Assets/Scripts/Player/boatScript.cs
+++ ProjectSushi/Assets/Scripts/Player/boatScript.cs
@@ -9,6 +9,12 @@ public class boatScript : MonoBehaviour {
     public GameObject boat;
     public GameObject VRhead;
 
+    public float paddlePower;
+    [Range(0, 1)]
+    public float paddleSideAngle = 0.2f;
+
+    Rigidbody rb;
+
     [Range(-2, 2)]
     public float movement;
     [Range(0,1)]
@@ -35,6 +41,19 @@ public class boatScript : MonoBehaviour {
                                   Quaternion.Euler(boat.transform.localRotation.x, boat.transform.localRotation.y, boat.transform.localRotation.z - movementValue * 15), 
                                   perc);
 
+    }
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void paddle(bool isLeft) {
+        if (isLeft) {
+            rb.AddForce(new Vector3(paddleSideAngle, 0, 1) * paddlePower, ForceMode.Impulse);
+        } else {
+            rb.AddForce(new Vector3(-paddleSideAngle, 0, 1) * paddlePower, ForceMode.Impulse);
+        }
+        print("Paddle is active");
     }
 
     // Update is called once per frame
